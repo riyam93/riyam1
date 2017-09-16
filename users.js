6bfb571a52577036db9c35108eeb9cf58d26be4f
riyam93 <32012600+riyam93@users.noreply.github.com>
2017-09-16 18:27:04 +0300

    users

---
+++ users.js
@@ -0,0 +1,4 @@
+{"users":[
+   {"id":1,"firstName":"Ahmed","lastName":"Zaki","phone":"55667788","mobile":"55776688","email":"Ahmed@yahoo.com","username":"Ahmed12","password":"password"
+   },
+   {"id":2,"firstName":"Nader","lastName":"Ahmed","phone":"44889977","mobile":"77669900","email":"Nader@yahoo.com","username":"Nader3","password":"password"}]}
