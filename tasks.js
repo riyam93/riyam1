2f31e0baf72533da6268adba5a8d73abb645c42a
riyam93 <32012600+riyam93@users.noreply.github.com>
2017-09-16 19:03:04 +0300

    tasks

---
+++ tasks.js
@@ -0,0 +1,4 @@
+{"tasks":[{"id":1,"Surah":1,"fromAya":1,"toAya":50,"type":"Revision","dueDate":"9/17/2017","completedDate":"9/16/2017","level":"Bigenner","comment":"Biggener revision","surahId":1,"userId":1},
+          {"id":2,"Surah":2,"fromAya":40,"toAya":90,"type":Memorizasion","dueDate":"9/20/2017","completedDate":"9/24/2017","level":"Medium","comment":"need more memorizasion","surahId":2,"userId":2}
+]}
+ 
